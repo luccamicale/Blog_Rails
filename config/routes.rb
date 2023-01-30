a69051efea3e528f4f13a10a8fbd1a05835c809b
luccamicale <luccamicale2015@gmail.com>
2023-01-30 13:22:14 -0300

    create controllers and file of view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  resource :users, only: %i[index show] do
+    resource :posts, only: %i[index show] do
+      # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+      # Defines the root path route ("/")
+      # root "articles#index"
+    end
+  end
 end
