5daade21577648e4da2581516c0e0384d8efa6b5
luccamicale <luccamicale2015@gmail.com>
2023-01-30 14:29:53 -0300

    modified the routes rest

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  resource :users, only: %i[index show] do
-    resource :posts, only: %i[index show] do
-      # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/users/:user_id/posts', to: 'posts#index'
+  get '/users/:user_id/posts/:id', to: 'posts#show'
 
-      # Defines the root path route ("/")
-      # root "articles#index"
-    end
-  end
+  resources :users, only: %i[index show]
 end
