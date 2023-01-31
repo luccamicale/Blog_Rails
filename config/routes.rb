a072ed35465faf9c985ba777a8878c7780f0feb7
luccamicale <luccamicale2015@gmail.com>
2023-01-31 14:45:25 -0300

    modified files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts, only: %i[index show]
+  root 'users#index'
 
-  resources :users, only: %i[index show]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
+  end
 end
