d1d070f4c70288233295b2aecd9f0fadf3ba7ecc
luccamicale <luccamicale2015@gmail.com>
2023-02-01 18:09:57 -0300

    create method in post cont and add new html post

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new]
   end
 end
