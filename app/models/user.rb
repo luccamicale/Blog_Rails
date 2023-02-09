3b39a28881b238541f0a4295a90bec78a3c926c8
luccamicale <luccamicale2015@gmail.com>
2023-02-08 22:45:49 -0300

    add method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
+  def is?(requested_role)
+    role == requested_role.to_s
+  end
+
   def latest_posts
     posts.order(created_at: :desc).limit(3)
   end
